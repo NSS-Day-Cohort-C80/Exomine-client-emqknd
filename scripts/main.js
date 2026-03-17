e58843807ce4ad680322e42e0dafde42d97e9454
Jude Andersen <jude.ottilie@gmail.com>
2026-03-17 15:09:19 -0500

    added basic css, will need edits once code is closer to complete

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,4 @@
-// import {} from './module.js'
 import { getFacilities, handleFacilityChoice } from "./facilities.js"
-import { getGovernors } from "./governors.js"
 //TODO: import other modules
 
 const mainContainer = document.querySelector("#container")
@@ -8,31 +6,30 @@ const mainContainer = document.querySelector("#container")
 const render = async () => {
 
     const mainHTML = `
-    <h1> Solar System Mining Marketplace </h1>
+    <h1>Solar System Mining Marketplace</h1>
     <article>
-	    <div>
-		    <section>
-    			Get Gov
-			</section>
-		    <section id="facility-container">
-			    ${await getFacilities()}
-		    </section>
-		    <section id="colonyMinerals-container">
-			    Colony Mineral List
-		    </section>
-	    </div>
-	    <div>
-		    <section id="facility-minerals-container">
-    		Facility Minerals radio buttons list
-			</section>
-		    <section>
-			    Space Cart Button
-		    </section>
-	    </div>
+        <div>
+            <section>
+                Choose Gov
+            </section>
+            <section id="facility-container">
+                ${await getFacilities()}
+            </section>
+            <section id="facility-minerals-container">
+                Facility Minerals radio buttons list
+            </section>
+        </div>
+        <div>
+            <section id="colonyMinerals-container">
+                Colony Mineral List
+            </section>
+            <section id="cart-container">
+                Space Cart Button
+            </section>
+        </div>
     </article>
-
     `
-        mainContainer.innerHTML = mainHTML
+    mainContainer.innerHTML = mainHTML
     handleFacilityChoice()
 }
 
