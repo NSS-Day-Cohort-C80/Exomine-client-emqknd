8ad9de0e2f01724d31132bcfb6b64a40d79c1e52
Brian Fuzzell <2788883+brianfuzzell@users.noreply.github.com>
2026-03-18 13:41:52 -0500

    Restoring main.js to the latest version that's on develop

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,16 @@
 // import {} from './module.js'
-import { governors, colonyMinerals } from "./governors.js"
 
 const mainContainer = document.querySelector("#container")
 
 const render = async () => {
-	const governorsHTML = await governors()
-	const colonyMineralsHTML = await colonyMinerals()
 
     const mainHTML = `
     <h1> Title </h1>
     <article>
 	    <div>
 		    <section>
-			    ${governorsHTML}
+			    Choose Gov
 		    </section>
-			<section>
-				${colonyMineralsHTML}
-			</section>
 		    <section>
 			    Choose Facility
 		    </section>
@@ -37,12 +31,4 @@ const render = async () => {
     `
     mainContainer.innerHTML = mainHTML
 }
-
-// Add an event listener for our new custom event
-// The DOM will re-render to reflect the new changes - new form submissions
-document.addEventListener("stateChanged", CustomEvent => {
-    // console.log("Data state changed. Regenerating HTML now.")
-    render()
-})
-
 render()
