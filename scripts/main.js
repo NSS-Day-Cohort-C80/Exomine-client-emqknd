6d6763ceaabaaf5dc02f6ee7f96dfc1f3dcd5cf0
Jude Andersen <jude.ottilie@gmail.com>
2026-03-19 16:42:18 -0500

    changed css stuff

--- scripts/main.js
+++ scripts/main.js
@@ -9,27 +9,30 @@ export const render = async () => {
 
     const mainHTML = `
     <h1>Solar System Mining Marketplace</h1>
-    <article>
-        <div class="left-col">
-            <section id="governor-container">
-                ${await governors()}
-            </section>
-            <section id="facility-container">
-                ${await getFacilities()}
-            </section>
-            <section id="facility-minerals-container">
-                ${await facilitySection()}
-            </section>
-        </div>
-        <div class="right-col">
-            <section id="colonyMinerals-container">
-                ${await colonyMinerals()}
-            </section>
-            <section id="cart-container">
-                ${await spaceCart()}
-                ${FinishButton()}
-            </section>
-        </div>
+<article>
+    <div class="top-row">
+        <section id="governor-container">
+            ${await governors()}
+        </section>
+        <section id="facility-container">
+            ${await getFacilities()}
+        </section>
+    </div>
+    <div class="top-row-right">
+        <section id="colonyMinerals-container">
+            ${await colonyMinerals()}
+        </section>
+    </div>
+</article>
+<div class="bottom-row">
+    <section id="facility-minerals-container">
+        ${await facilitySection()}
+        </section>
+        <section id="cart-container">
+        ${await spaceCart()}
+        ${FinishButton()}
+        </section>
+</div>
     </article>`
     
     mainContainer.innerHTML = mainHTML
